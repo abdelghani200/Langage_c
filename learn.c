11cc35b9d6c0ffb5802b62ef07578e983e2da8eb
abdelghani200 <83412300+abdelghani200@users.noreply.github.com>
2022-09-07 11:42:45 +0200

    if-else

--- learn.c
+++ learn.c
@@ -15,4 +15,26 @@ Un nom : c'est ce qui permet de la reconnaître */
   printf("************\n");
   printf("%d\n",a);
   printf("%d\n",b);
+  int x;
+  x=a++;
+  printf("%d\n",x);
+  
+  /* La structure des conditions (if-else):
+    Structure:
+    if(condition)                                      si condition est vrai (≠0)
+    {                                                   on exécute
+    bloc d'instructions 1;                             bloc d'instructions 1
+    }
+    else sinon
+    {                                                    on exécute
+    bloc d'instructions 2;                            bloc d'instructions 2 
+    }
+    */
+  int max;
+  if(a>b){
+    max=a;
+  } else  max=b;
+  printf("Le max de a et b est :%d",max);
+  
+  
 }
