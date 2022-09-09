2c983d1ad2242cebda559bcd64541deef953ff5c
Jelouani code <jelouaniabdelghani@gmail.com>
2022-09-09 11:32:53 +0200

    scientifique_calcule

--- calculatrice.c
+++ calculatrice.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<math.h>
 int main(){
-   int A,B;
+   float A,B;
    int c;
    char reponse;
    
@@ -13,28 +13,28 @@ int main(){
    	printf("2-substraction\n");
    	printf("3-multiplication \n");
    	printf("4_division \n");
-        printf("5-puissance\n");
+   	printf("5-puissance\n");
    	printf("Quelle calcule veut-tu effectuer?\n");
    	scanf("%d",&c);
    	printf("Enter un nombre:\n");
-   	scanf("%d",&A);
+   	scanf("%f",&A);
    	printf("Enter un autre nombre:\n");
-   	scanf("%d",&B);
+   	scanf("%f",&B);
    	switch(c){
-   		case 1 :printf("Le resultat est :%d\n",A+B);
+   		case 1 :printf("Le resultat est :%f\n",A+B);
 		        break;
-   		case 2 :printf("Le resultat est :%d\n",A-B);
+   		case 2 :printf("Le resultat est :%f\n",A-B);
 		        break;
-   		case 3 :printf("Le resultat est :%d\n",A*B);
+   		case 3 :printf("Le resultat est :%f\n",A*B);
 		        break;
-   		case 4 :if(A!=0){
-   			printf("Le resultat est :%d\n",A/B);
+   		case 4 :if(B!=0){
+   			printf("Le resultat est :%f\n",A/B);
 		   }
-		   else if(A==0){
+		   else if(B==0){
 		   	printf("incorrect...\n");
 		   };
 		   break;
-		case 5 :printf("Le resultat est : %f\n",pow(A,B));break;   
+		case 5 :printf("Le resultat est : %f\n",pow(A,B));break;
 		   
 	default : printf("erreur\n"); break;  
     }
