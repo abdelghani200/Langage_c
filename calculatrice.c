3cd4c7718147c0d5fce5f68bece24cc62664e128
Jelouani code <jelouaniabdelghani@gmail.com>
2022-09-09 17:35:33 +0200

    cos_sin_tan

--- calculatrice.c
+++ calculatrice.c
@@ -1,40 +1,83 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+
+static int i;
+
 int main(){
-   float A,B;
+   float A,B,y;
    int c;
    char reponse;
    
    	do{
    		
-   	printf("\t\t Calculatrice\n ");
-   	printf("1-additon \n");
-   	printf("2-substraction\n");
-   	printf("3-multiplication \n");
-   	printf("4_division \n");
-   	printf("5-puissance\n");
-   	printf("Quelle calcule veut-tu effectuer?\n");
-   	scanf("%d",&c);
-   	printf("Enter un nombre:\n");
-   	scanf("%f",&A);
-   	printf("Enter un autre nombre:\n");
-   	scanf("%f",&B);
+	   	printf("\t\t Calculatrice\n");
+	   	printf("1-additon\n");
+	   	printf("2-substraction\n");
+	   	printf("3-multiplication \n");
+	   	printf("4_division \n");
+	   	printf("5-puissance\n");
+	   	printf("6-racine\n");
+	   	printf("7-cousinus\n");
+	   	printf("8-sinis\n");
+	   	printf("9-tangante\n");
+	   	printf("Quelle calcule veut-tu effectuer?\n");
+	   	scanf("%d",&c);
    	switch(c){
-   		case 1 :printf("Le resultat est :%f\n",A+B);
+   		case 1 :printf("Enter un nombre:\n");
+			   	scanf("%f",&A);
+			   	printf("Enter un autre nombre:\n");
+			   	scanf("%f",&B);
+		        printf("Le resultat est :%f\n",A+B);
 		        break;
-   		case 2 :printf("Le resultat est :%f\n",A-B);
+   		case 2 :printf("Enter un nombre:\n");
+			   	scanf("%f",&A);
+			   	printf("Enter un autre nombre:\n");
+			   	scanf("%f",&B);
+		        printf("Le resultat est :%f\n",A-B);
 		        break;
-   		case 3 :printf("Le resultat est :%f\n",A*B);
+   		case 3 :
+				printf("Enter un nombre:\n");
+			   	scanf("%f",&A);
+			   	printf("Enter un autre nombre:\n");
+			   	scanf("%f",&B);
+			    printf("Le resultat est :%f\n",A*B);
 		        break;
-   		case 4 :if(B!=0){
-   			printf("Le resultat est :%f\n",A/B);
-		   }
-		   else if(B==0){
-		   	printf("incorrect...\n");
-		   };
-		   break;
-		case 5 :printf("Le resultat est : %f\n",pow(A,B));break;
+   		case 4 :printf("Enter un nombre:\n");
+			   	scanf("%f",&A);
+			   	printf("Enter un autre nombre:\n");
+			   	scanf("%f",&B);
+			    if(B!=0){
+	   			printf("Le resultat est :%f\n",A/B);
+			   }
+			   else if(B==0){
+			   	printf("incorrect...\n");
+			   };
+			   break;
+		case 5 :printf("Enter un nombre:\n");
+			   	scanf("%f",&A);
+			   	printf("Enter un autre nombre:\n");
+			   	scanf("%f",&B);
+				
+				printf("Le resultat est : %f\n",pow(A,B));break;
+		case 6 :printf("Donner un nombre:\n");
+			    scanf("%f",&i);
+			    printf("rasine(%f)=%f\n",i,sqrt(i));;break;
+		
+		
+		case 7 :printf("Donner un nombre:\n");
+			    scanf("%f",&i);
+			    printf("Cosinus(%f)=%f\n",i,cos(i));break;
+		
+		
+		case 8 :printf("Donner un nombre:\n");
+			    scanf("%f",&i);
+			    printf("sinus(%f)=%f\n",i,sin(i));;break;
+		
+		
+		case 9 :printf("Donner un nombre:\n");
+			    scanf("%f",&i);
+			    printf("tan(%f)=%f\n",i,tan(i));;break;
 		   
 	default : printf("erreur\n"); break;  
     }
